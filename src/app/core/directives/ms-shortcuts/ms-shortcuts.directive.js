c99fbbfb078f110c9b5a8879fa6606ad9d420689
nenad <nesa993@gmail.com>
2016-07-01 18:39:58 +0200

    bookmarking added

--- src/app/core/directives/ms-shortcuts/ms-shortcuts.directive.js
+++ src/app/core/directives/ms-shortcuts/ms-shortcuts.directive.js
@@ -20,6 +20,7 @@
         vm.resultsLoading = false;
         vm.selectedResultIndex = 0;
         vm.ignoreMouseEvents = false;
+        vm.mobileBarActive = false;
 
         vm.results = null;
         vm.shortcuts = [];
@@ -47,6 +48,7 @@
         vm.handleMouseenter = handleMouseenter;
         vm.temporarilyIgnoreMouseEvents = temporarilyIgnoreMouseEvents;
         vm.ensureSelectedResultIsVisible = ensureSelectedResultIsVisible;
+        vm.toggleMobileBar = toggleMobileBar;
 
         //////////
 
@@ -201,38 +203,14 @@
             {
                 shortcuts = [
                     {
-                        'title'      : 'Chat',
-                        'icon'       : 'icon-hangouts',
-                        'state'      : 'app.chat',
-                        'badge'      : {
-                            'content': 13,
-                            'color'  : '#09d261'
-                        },
-                        'weight'     : 5,
+                        'title'      : 'Sample',
+                        'icon'       : 'icon-tile-four',
+                        'state'      : 'app.sample',
+                        'weight'     : 1,
                         'children'   : [],
-                        '_id'        : 'chat',
-                        '_path'      : 'apps.chat',
-                        'uisref'     : 'app.chat',
-                        'hasShortcut': true
-                    }, {
-                        'title'      : 'Contacts',
-                        'icon'       : 'icon-account-box',
-                        'state'      : 'app.contacts',
-                        'weight'     : 10,
-                        'children'   : [],
-                        '_id'        : 'contacts',
-                        '_path'      : 'apps.contacts',
-                        'uisref'     : 'app.contacts',
-                        'hasShortcut': true
-                    }, {
-                        'title'      : 'Notes',
-                        'icon'       : 'icon-lightbulb',
-                        'state'      : 'app.notes',
-                        'weight'     : 11,
-                        'children'   : [],
-                        '_id'        : 'notes',
-                        '_path'      : 'apps.notes',
-                        'uisref'     : 'app.notes',
+                        '_id'        : 'sample',
+                        '_path'      : 'apps.sample',
+                        'uisref'     : 'app.sample',
                         'hasShortcut': true
                     }
                 ];
@@ -484,6 +462,14 @@
                     resultsEl.scrollTop(bottom - resultsEl.height());
                 }
             }
+        }
+
+        /**
+         * Toggle mobile bar
+         */
+        function toggleMobileBar()
+        {
+            vm.mobileBarActive = !vm.mobileBarActive;
         }
     }
 
