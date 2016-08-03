8fca6c9a1c896b535f472b141074d360313e31b8
nenad <nesa993@gmail.com>
2016-08-03 14:26:15 +0200

    credit card info error handling and validation

--- src/app/main/dashboard/dialogs/create/createCommunity.controller.js
+++ src/app/main/dashboard/dialogs/create/createCommunity.controller.js
@@ -28,12 +28,10 @@
 
       apilaData.hasCanceledCommunity(vm.userid)
       .success(function(response) {
-        console.log(response);
         vm.canceledCommunity = response;
         vm.hasCanceledCommunity = true;
       })
       .error(function(response) {
-        console.log(response);
       });
 
       function closeDialog()
@@ -61,41 +59,60 @@
         console.log(vm.cardInfo);
         console.log(vm.userid);
 
+        if(!vm.form.name) {
+          vm.nameError = "Community name must be specified";
+          return;
+        }
+
         Stripe.card.createToken(vm.cardInfo,
           function(status, response) {
 
             console.log(response);
 
             if(status !== 200) {
-              showErrorToast(response.error.message);
+            //  showErrorToast(response.error.message);
+              processErrors(response.error);
+            } else {
+              apilaData.saveCreditCard(vm.userid, response)
+              .success(function(response) {
+                console.log(response);
+
+                apilaData.addCommunity(vm.form)
+                .success(function(d) {
+                  closeDialog();
+
+                  $mdToast.show(
+                    $mdToast.simple()
+                      .textContent("Community has been created!")
+                      .position("top right")
+                      .hideDelay(2200)
+                  );
+                })
+                .error(function(d) {
+                  console.log("Error while creating community");
+                });
+              })
+              .error(function(response) {
+                console.log(response);
+              });
             }
 
-            apilaData.saveCreditCard(vm.userid, response)
-            .success(function(response) {
-              console.log(response);
 
-              apilaData.addCommunity(vm.form)
-              .success(function(d) {
-                closeDialog();
-
-                $mdToast.show(
-                  $mdToast.simple()
-                    .textContent("Community has been created!")
-                    .position("top right")
-                    .hideDelay(2200)
-                );
-              })
-              .error(function(d) {
-                console.log("Error while creating community");
-              });
-            })
-            .error(function(response) {
-              console.log(response);
-            });
           });
 
       }
 
+
+      function processErrors(error) {
+        if(error.param === "card[number]") {
+          vm.cardNumberError = "Invalid credit card number";
+        } else if(error.param === "card[exp_month]") {
+          vm.expMonthError = "Invalid Expiration Month entered please select between 01 - 12";
+        } else if(error.param === "card[exp_year]") {
+          vm.expYearError = "Invalid Expiration Year entered";
+        }
+
+      }
 
       function recoverCommunity() {
         Stripe.card.createToken(vm.cardInfo,
@@ -104,38 +121,42 @@
             console.log(response);
 
             if(status !== 200) {
-              showErrorToast(response.error.message);
-            }
-
-            apilaData.saveCreditCard(vm.userid, response)
-            .success(function(response) {
-              console.log(response);
-
-              //restore community
-              apilaData.restoreCommunity(vm.userid, vm.canceledCommunity._id)
+              //showErrorToast(response.error.message);
+              processErrors(response.error);
+            } else {
+              apilaData.saveCreditCard(vm.userid, response)
               .success(function(response) {
                 console.log(response);
 
-                closeDialog();
+                //restore community
+                apilaData.restoreCommunity(vm.userid, vm.canceledCommunity._id)
+                .success(function(response) {
+                  console.log(response);
 
-                $mdToast.show(
-                  $mdToast.simple()
-                    .textContent("Community has been restored!")
-                    .position("top right")
-                    .hideDelay(2200)
-                );
+                  closeDialog();
 
+                  $mdToast.show(
+                    $mdToast.simple()
+                      .textContent("Community has been restored!")
+                      .position("top right")
+                      .hideDelay(2200)
+                  );
+
+                })
+                .error(function(response) {
+                  console.log(response);
+                });
               })
               .error(function(response) {
-                console.log(response);
-              });
-            })
-            .error(function(response) {
 
-            });
+              });
+            }
 
           });
       }
+
+
+
 
   }
 
