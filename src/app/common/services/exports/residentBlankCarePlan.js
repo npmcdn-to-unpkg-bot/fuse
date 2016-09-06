c44454ede876eb8b35704efe69160908baddd7fc
KyleRiggen <kyle.riggenbach@gmail.com>
2016-09-06 04:00:27 -0600

    test commit to see if still rogue commits

--- src/app/common/services/exports/residentBlankCarePlan.js
+++ src/app/common/services/exports/residentBlankCarePlan.js
@@ -13,11 +13,24 @@
 
       doc.setFont("helvetica");
       doc.setFontSize(12);
+      doc.setLineWidth(1);
 
       // community logo
       doc.rect(250, 10, 100, 100);
       doc.text("place holder", 260, 50);
       doc.text("for logo", 260, 62);
+
+      doc.text("Phone", 360, 24);
+      doc.text("(719) 587-3514", 410, 24);
+
+      doc.text("Fax", 360, 36);
+      doc.text("(719) 589-3614", 410, 36);
+
+      doc.text("Address", 360, 48);
+      doc.text("3407 Carroll St Alamoa CO, 81101", 410, 48);
+
+      doc.text("Website", 360, 60);
+      doc.text("AlamosaBridge.com", 410, 60);
 
       // resident picture
       doc.rect(10, 120, 200, 300);
@@ -25,11 +38,32 @@
       doc.text("for resident picture", 60, 262);
 
       // resident admin info
-      doc.text("First Name", 220, 130);
-      doc.text("Prefered Name", 220, 142);
-      doc.text("Middle Name", 220, 154);
-      doc.text("Last Name", 220, 166);
-      doc.text("First Name", 220, 178);
+      doc.text("First Name", 220, 150);
+      doc.line(280, 152, 590, 152);
+
+      doc.text("Preferred Name", 220, 174);
+      doc.line(306, 176, 590, 176);
+
+      doc.text("Middle Name", 220, 198);
+      doc.line(291, 200, 590, 200);
+
+      doc.text("Last Name", 220, 222);
+      doc.line(280, 224, 590, 224);
+
+      doc.text("Maiden Name", 220, 246);
+      doc.line(297, 248, 590, 248);
+
+      doc.text("Room", 220, 270);
+      doc.text("Sex", 390, 270);
+
+      doc.text("Date of Birth", 220, 294);
+      doc.text("Admission Date", 390, 294);
+
+      doc.text("Marital Status", 220, 318);
+      doc.text("Veteran", 390, 318);
+
+      doc.text("Full Code", 220, 324);
+      doc.text("Assessment Interval", 390, 324);
 
       doc.save(resident.firstName + " " + resident.lastName + ".pdf");
     }
